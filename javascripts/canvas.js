371a9d4e314152b3c40b7afa3442b85948cdf4f5
huyle333 <huyle333@bu.edu>
2016-01-23 03:00:01 -0500

    [HL] Testing touch movement button controls.

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -47,35 +47,35 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
-document.getElementById("up").addEventListener("mousedown", function(){
+document.getElementById("up").addEventListener("touchstart", function(){
 	keysDown[38] = true;
 }, false);
 
-document.getElementById("up").addEventListener("mouseup", function(){
+document.getElementById("up").addEventListener("touchend", function(){
 	delete keysDown[38];
 }, false);
 
-document.getElementById("left").addEventListener("mousedown", function(){
+document.getElementById("left").addEventListener("touchstart", function(){
 	keysDown[37] = true;
 }, false);
 
-document.getElementById("left").addEventListener("mouseup", function(){
+document.getElementById("left").addEventListener("touchend", function(){
 	delete keysDown[37];
 }, false);
 
-document.getElementById("right").addEventListener("mousedown", function(){
+document.getElementById("right").addEventListener("touchstart", function(){
 	keysDown[39] = true;
 }, false);
 
-document.getElementById("right").addEventListener("mouseup", function(){
+document.getElementById("right").addEventListener("touchend", function(){
 	delete keysDown[39];
 }, false);
 
-document.getElementById("down").addEventListener("mousedown", function(){
+document.getElementById("down").addEventListener("touchstart", function(){
 	keysDown[40] = true;
 }, false);
 
-document.getElementById("down").addEventListener("mouseup", function(){
+document.getElementById("down").addEventListener("touchend", function(){
 	delete keysDown[40];
 }, false);
 
